148bf4cb3b4c678e821dacce8e04d76afe856ff0
Shivansh <shivanshmishra092003@gmail.com>
2026-04-07 00:03:38 +0530

    display notes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.get("/",(req,res)=>{
-  res.send("API Running...");
+
+// Dummy data
+let notes = [
+  { id: 1, text: "Learn React" },
+  { id: 2, text: "Build project" }
+];
+
+// GET notes
+app.get("/notes", (req, res) => {
+  res.json(notes);
 });
-app.listen(5001,()=>{
-  console.log("Server running on port 5000");
-})
+
+app.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
